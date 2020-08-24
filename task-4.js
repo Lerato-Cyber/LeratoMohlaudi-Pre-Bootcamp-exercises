deea0bb68efaed7ec50ca012d858ccaaf89dda50
Lerato-Cyber <69108772+Lerato-Cyber@users.noreply.github.com>
2020-08-24 09:33:11 +0200

    task-4

--- task-4.js
+++ task-4.js
@@ -1,6 +1,9 @@
 
-const see = (x,y) => x === 3 || y === 3 || (x + y) ===3;
-
+   
+   
+function see (x,y) {
+   return (( x == 3 || y == 3) || (x + y ==3));
+}
 console.log(see(9,9));
 console.log(see(1,2));
 
