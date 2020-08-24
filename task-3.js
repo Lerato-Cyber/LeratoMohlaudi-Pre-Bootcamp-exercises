933879dbd245a60cda023310fd3e69c469a57dda
Lerato-Cyber <69108772+Lerato-Cyber@users.noreply.github.com>
2020-08-24 16:41:00 +0200

    task 3

--- task-3.js
+++ task-3.js
@@ -1,7 +1,7 @@
-//Task 3
 
-let number = 65;
-if (number >= 65){console.log (true);}
-else if (number <65) {console.log (false);}
-
+function numbersBool(x, y){ 
+    if  ((x == 65) || (y == 65) || ((x+y == 65))){console.log("true")}
+    else {console.log("false")}
+    }
+    numbersBool( 65+15)
 
